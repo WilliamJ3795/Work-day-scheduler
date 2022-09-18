b8b27a0b048b31de8c076b09b988f595b672e0da
William James <Williamj3795@gmail.com>
2022-09-18 19:13:09 -0400

    created for loop

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,32 @@
-// Display today's day and date
-var todayDate = moment().format('dddd, MMM Do YYYY');
-$("#currentDay").html(todayDate);
+var saveBtn = document.querySelectorAll(".saveBtn");
+var currentHour = moment().hour();
+var militaryHour = [8, 9, 10, 11, 12, 13, 14, 15, 16];
 
-$(document).ready(function () {
-    // saveBtn click listener 
-    $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+console.log(currentHour);
 
-        // Save text in local storage
-        localStorage.setItem(time, text);
-    })
-   
-   
-    })
+document.querySelector("#currentDay").textContent = moment().format("dddd, MMMM Do YYYY");
+
+for (var i = 0; i < 9; i++) {
+    // if current hour strictly matches the time block, mark time block with present color
+    if (currentHour === militaryHour[i]) {
+      console.log(i)
+      document.getElementById(i).classList.add("present");
+    }
+// if  currentHour is greater than the time block, mark time block with past color
+    if (currentHour > militaryHour[i]) {
+        console.log(i);
+        document.getElementById(i).classList.add("past");
+      }
+// if currentHour is less than the time block...
+    if (currentHour < militaryHour[i]) {
+        console.log(i);
+        document.getElementById(i).classList.add("future");
+    }
+    // gets 
+    if(localStorage.getItem(i)){
+        document.getElementById(i).value = localStorage.getItem(i);
+        console.log(localStorage)
+        console.log(getItem)
+    }
+
+}
